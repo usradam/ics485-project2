fd1bbcdb17423509cc73c08dd216724dda572359
Adam <147444558+usradam@users.noreply.github.com>
2026-03-17 21:14:01 -1000

    Setup animations
    Have player animations for being idle, moving, and jumping.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,53 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        // Grab references for rigidbody and animator from game object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
 
-        if(Input.GetKey(KeyCode.Space))
+        // Flip player when moving left and right
+        if (horizontalInput > 0.01f)
         {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+            transform.localScale = new Vector3(5, 5, 5);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-5, 5, 5);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        // Set animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
         }
     }
 }
