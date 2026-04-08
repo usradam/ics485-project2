25f77bef7b541f93daeb5ff0322f60af327cdc30
Adam <147444558+usradam@users.noreply.github.com>
2026-04-07 23:10:21 -1000

    Implement shooting mechanics

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform bulletPoint;
+    [SerializeField] private GameObject[] bullets;
     private Animator anim;
     private PlayerMovement playerMovement;
     private float cooldownTimer = Mathf.Infinity;
@@ -15,17 +17,31 @@ public class PlayerAttack : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
+        if (Input.GetKeyDown(KeyCode.F) && cooldownTimer > attackCooldown && playerMovement.canAttack())
         {
             Attack();
         }
         cooldownTimer += Time.deltaTime;
     }
-    
+
     private void Attack()
     {
         anim.SetTrigger("attack");
         cooldownTimer = 0;
-        
+
+        bullets[FindBullet()].transform.position = bulletPoint.position;
+        bullets[FindBullet()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+    
+    private int FindBullet()
+    {
+        for (int i = 0; i < bullets.Length; i++)
+        {
+            if (!bullets[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
     }
 }
