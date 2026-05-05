830aaadf820e6486a8b89da7d90f18671e7b403c
Adam <147444558+usradam@users.noreply.github.com>
2026-05-05 13:02:58 -1000

    Set up enemy

--- Assets/Scripts/Traps/EnemyDamage.cs
+++ Assets/Scripts/Traps/EnemyDamage.cs
@@ -6,7 +6,7 @@ public class EnemyDamage : MonoBehaviour
 
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
             collision.GetComponent<Health>().TakeDamage(damage);
     }
 }
